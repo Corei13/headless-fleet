38f7eeeb7ce817759d8f4c41de47d13b81a5a3fb
Dhananjoy Biswas <ebappa1971@gmail.com>
2018-07-23 15:42:35 +0600

    Marriage is the sole cause of divorce.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,19 +6,20 @@ curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 
 apt-get update
 apt-get install -y git nodejs build-essential tcl supervisor htop
+npm i -g yarn
 
 cd /home/ubuntu/
 git clone https://github.com/Corei13/$REPO.git
 cd $REPO
-npm install
-npm run build
+yarn
+yarn build
 
 service supervisor stop
 
 cat > /etc/supervisor/conf.d/$REPO.conf << EOL
 [program:${REPO}]
 directory = /home/ubuntu/${REPO}
-command = npm run worker
+command = yarn worker
 autorestart = true
 stdout_logfile = /log/worker.log
 stderr_logfile = /log/worker.err
