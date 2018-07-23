c382489edf49369c26ef99ea74d7c0f215c79312
Dhananjoy Biswas <ebappa1971@gmail.com>
2018-07-23 15:43:47 +0600

    Fine day for friends. So-so day for you.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,7 +5,7 @@ REPO=headless-fleet
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 
 apt-get update
-apt-get install -y git nodejs build-essential tcl supervisor htop
+apt-get install -y git nodejs build-essential tcl supervisor htop libx11-6 libx11-xcb1 libxcomposite1 libxcursor1 libxdamage1 libpango-1.0-0 libpangocairo-1.0-0 libnspr4 libcairo2 libcups2 libxss1 libasound2 libatk1.0-0 libgtk-3-0 libnss3
 npm i -g yarn
 
 cd /home/ubuntu/
